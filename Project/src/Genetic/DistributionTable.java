fb82dd601a37953b90dda87e0a87ca58537ab915
Ixel Castro <ixcastro@ic-itcr.ac.cr>
2020-07-09 14:37:55 -0600

    GA Correction
    Correction in genetic algorithm

--- Project/src/Genetic/DistributionTable.java
+++ Project/src/Genetic/DistributionTable.java
@@ -4,218 +4,85 @@ import ImageManagment.Pixel;
 
 import java.awt.*;
 import java.text.DecimalFormat;
-import java.util.HashMap;
 import java.util.Vector;
 
 import lib.ICONSTANTS;
 
 public class DistributionTable implements ICONSTANTS{
-    Vector<ColorNode> distributionPetal;
-    Vector<ColorNode> distributionCenter;
-    Vector<Color> mainColorPetal;
-    Vector<Color> mainColorCenter;
-    Vector<Integer> totalPetal;
-    Vector<Integer> totalCenter;
+    ColorNode distributionPetal;
+    ColorNode distributionCenter;
+    int totalPetal;
+    int totalCenter;
 
     public DistributionTable(){
-        distributionPetal = new Vector<>();
-        distributionCenter = new Vector<>();
-        totalCenter = new Vector<>();
-        totalPetal = new Vector<>();
-        mainColorCenter = new Vector<>();
-        mainColorPetal = new Vector<>();
+        distributionPetal = new ColorNode();
+        distributionCenter = new ColorNode();
+        totalCenter = 0;
+        totalPetal = 0;
     }
 
     public void insertDistributionPetal(Vector<Pixel> flowerPixels){
-        distributionPetal.add(new ColorNode());
-        totalPetal.add(0);
         for(Pixel pix : flowerPixels){
-            insertColorPetal(pix.color);
+            distributionPetal.insertColor(pix.color);
+            totalPetal++;
         }
     }
 
     public void insertDistributionCenter(Vector<Pixel> flowerPixels){
-        distributionCenter.add(new ColorNode());
-        totalCenter.add(0);
         for(Pixel pix : flowerPixels){
-            insertColorCenter(pix.color);
+            distributionCenter.insertColor(pix.color);
+            totalCenter++;
         }
     }
 
-
-    public void insertColorPetal(Color color){
-        if (!distributionPetal.isEmpty()){
-            for (Color colorNode : distributionPetal.lastElement().colorInformation.keySet()) {
-                if (isSimilar(colorNode, color)) {
-                    distributionPetal.lastElement().colorInformation.get(colorNode).addToFrequency();
-                    totalPetal.setElementAt(totalPetal.lastElement()+1, totalPetal.size()-1);
-                    return;
-                }
-            }
-        }
-        distributionPetal.lastElement().colorInformation.put(color,new InfoNode());
-        totalPetal.setElementAt(totalPetal.lastElement()+1, totalPetal.size()-1);
-    }
-
-    public void insertColorCenter(Color color){
-        if (!distributionCenter.isEmpty()){
-            for (Color distColor : distributionCenter.lastElement().colorInformation.keySet()) {
-                if (isSimilar(distColor, color)) {
-                    //System.out.println("Added Freq");
-                    distributionCenter.lastElement().colorInformation.get(distColor).addToFrequency();
-                    totalCenter.setElementAt(totalCenter.lastElement()+1, totalCenter.size()-1);
-                    return;
-                }
-            }
-        }
-        //System.out.println("Added color");
-        distributionCenter.lastElement().colorInformation.put(color,new InfoNode());
-        totalCenter.setElementAt(totalCenter.lastElement()+1, totalCenter.size()-1);
-    }
-
-    public void setMainColor(){
-        if(!distributionPetal.isEmpty()){
-            for(ColorNode colorNode : distributionPetal){
-                Color mainColor = (Color)colorNode.mainColor();
-                mainColorPetal.add(mainColor);
-            }
-        }
-        if(!distributionCenter.isEmpty()){
-            for(ColorNode colorNode : distributionCenter){
-                Color mainColor = (Color)colorNode.mainColor();
-                mainColorCenter.add(mainColor);
-            }
-        }
-    }
-
-    public boolean isSimilar(Color color1, Color color2){
-        if(color1!=null && color2!=null){
-            float distance = (float) Math.sqrt(Math.pow(color1.getRed()-color2.getRed(),2) +
-                    Math.pow(color1.getGreen()-color2.getGreen(),2) +
-                    Math.pow(color1.getBlue()-color2.getBlue(),2));
-            if(distance/COLOR_BASE <= PERCENTAGE_COLOR){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public float similarity(Color color1, Color color2){
-        if(color1!=null && color2!=null){
-            float distance = (float) Math.sqrt(Math.pow(color1.getRed()-color2.getRed(),2) +
-                    Math.pow(color1.getGreen()-color2.getGreen(),2) +
-                    Math.pow(color1.getBlue()-color2.getBlue(),2));
-            //System.out.println(distance/COLOR_BASE); //Percetage
-            return distance/COLOR_BASE;
-        }
-        return 0;
-    }
-
-    public void setPercentages(){
-        DecimalFormat df = new DecimalFormat("#.####");
-        float percentage = 0;
-        float frequency = 0;
-        for(int i=0; i<distributionPetal.size(); i++){
-            for(Color petalColor : distributionPetal.elementAt(i).colorInformation.keySet()){
-                frequency = distributionPetal.elementAt(i).colorInformation.get(petalColor).getFrequency();
-                percentage = Float.parseFloat(df.format(frequency/totalPetal.elementAt(i)));
-                distributionPetal.elementAt(i).colorInformation.get(petalColor).setPercentage(percentage);
-            }
-        }
-        for(int i=0; i<distributionCenter.size(); i++){
-            for(Color petalColor : distributionCenter.elementAt(i).colorInformation.keySet()){
-                frequency = distributionCenter.elementAt(i).colorInformation.get(petalColor).getFrequency();
-                percentage = Float.parseFloat(df.format(frequency/totalCenter.elementAt(i)));
-                //System.out.println("F: " + frequency + " T: " + totalCenter.elementAt(i));
-                distributionCenter.elementAt(i).colorInformation.get(petalColor).setPercentage(percentage);
-            }
-        }
-    }
-
-    public void setMinMaxValues(){
-        int minValue = 0;
-        for(int i=0; i<distributionPetal.size(); i++){
-            for(Color petalColor : distributionPetal.elementAt(i).colorInformation.keySet()){
-                float percentage = distributionPetal.elementAt(i).colorInformation.get(petalColor).getPercentage();
-                distributionPetal.elementAt(i).colorInformation.get(petalColor).setMin(minValue);
-                minValue += (int) (percentage*MAX_CHROMOSOME_VALUE);
-                distributionPetal.elementAt(i).colorInformation.get(petalColor).setMax(minValue);
-                minValue++;
-            }
-        }
-        minValue = 0;
-        for(int i=0; i<distributionCenter.size(); i++){
-            for(Color centerColor : distributionCenter.elementAt(i).colorInformation.keySet()){
-                float percentage = distributionCenter.elementAt(i).colorInformation.get(centerColor).getPercentage();
-                //System.out.println(percentage);
-                distributionCenter.elementAt(i).colorInformation.get(centerColor).setMin(minValue);
-                minValue += (int) (percentage*MAX_CHROMOSOME_VALUE);
-                distributionCenter.elementAt(i).colorInformation.get(centerColor).setMax(minValue);
-                minValue++;
-            }
-        }
+    public void setTableValues(){
+        distributionPetal.setPercentages(totalPetal);
+        distributionCenter.setPercentages(totalCenter);
+        distributionPetal.setMinMaxValue();
+        distributionCenter.setMinMaxValue();
     }
 
     public void printTable(){
         //System.out.println(this.totalCenter);
-        for(ColorNode colorNode : distributionCenter){
-            colorNode.printNode();
-        }
+        System.out.println("Center colors");
+        distributionCenter.printNode();
         System.out.println();
-        /*System.out.println();
-        System.out.println(this.totalPetal);*/
-        for(ColorNode colorNode : distributionPetal){
-            colorNode.printNode();
-        }
+        System.out.println("Petal colors");
+        distributionPetal.printNode();
+        System.out.println();
     }
 
-    //***************************Setters and Getters **************************************************
+    //***************************Setters and Getters ******************************************
 
-    public Vector<ColorNode> getDistributionPetal() {
+    public ColorNode getDistributionPetal() {
         return distributionPetal;
     }
 
-    public void setDistributionPetal(Vector<ColorNode> distributionPetal) {
+    public void setDistributionPetal(ColorNode distributionPetal) {
         this.distributionPetal = distributionPetal;
     }
 
-    public Vector<ColorNode> getDistributionCenter() {
+    public ColorNode getDistributionCenter() {
         return distributionCenter;
     }
 
-    public void setDistributionCenter(Vector<ColorNode> distributionCenter) {
+    public void setDistributionCenter(ColorNode distributionCenter) {
         this.distributionCenter = distributionCenter;
     }
 
-    public Vector<Color> getMainColorPetal() {
-        return mainColorPetal;
-    }
-
-    public void setMainColorPetal(Vector<Color> mainColorPetal) {
-        this.mainColorPetal = mainColorPetal;
-    }
-
-    public Vector<Color> getMainColorCenter() {
-        return mainColorCenter;
-    }
-
-    public void setMainColorCenter(Vector<Color> mainColorCenter) {
-        this.mainColorCenter = mainColorCenter;
-    }
-
-    public Vector<Integer> getTotalPetal() {
+    public int getTotalPetal() {
         return totalPetal;
     }
 
-    public void setTotalPetal(Vector<Integer> totalPetal) {
+    public void setTotalPetal(int totalPetal) {
         this.totalPetal = totalPetal;
     }
 
-    public Vector<Integer> getTotalCenter() {
+    public int getTotalCenter() {
         return totalCenter;
     }
 
-    public void setTotalCenter(Vector<Integer> totalCenter) {
+    public void setTotalCenter(int totalCenter) {
         this.totalCenter = totalCenter;
     }
 }
--- Project/src/Genetic/GeneticAlgorithm.java
+++ Project/src/Genetic/GeneticAlgorithm.java
@@ -10,10 +10,10 @@ import java.util.Random;
 import java.util.Vector;
 
 public class GeneticAlgorithm implements ICONSTANTS {
-    public Vector<Population> centerPopulation;
-    public Vector<Population> petalPopulation;
-    public Vector<Vector<Integer>> aptPetal;
-    public Vector<Vector<Integer>> aptCenter;
+    public Vector<Integer> centerPopulation;
+    public Vector<Integer> petalPopulation;
+    public Vector<Integer> aptPetal;
+    public Vector<Integer> aptCenter;
 
     public GeneticAlgorithm(){
         aptPetal = new Vector<>();
@@ -22,111 +22,57 @@ public class GeneticAlgorithm implements ICONSTANTS {
         centerPopulation = new Vector<>();
     }
 
-    public void generatePopulation(DistributionTable distributionTable){
+    public void generatePopulation(){
         Random rand = new Random();
-        for(int i=0; i<distributionTable.distributionPetal.size(); i++){
-            petalPopulation.add(new Population());
-            aptPetal.add(new Vector<>());
-            for(int j=0; j<POPULATION_SIZE; j++){
-                int individual = rand.nextInt(MAX_CHROMOSOME_VALUE + 1);
-                Color chromosomeColor = distributionTable.distributionPetal.elementAt(i).getValueOf(individual);
-                if(chromosomeColor!=null){
-                    Color mainColor = distributionTable.distributionPetal.elementAt(i).mainColor();
-                    if(isMainColor(mainColor,chromosomeColor)){
-                        //aptPetal.add(chromosomeColor);
-                        aptPetal.elementAt(i).add(individual);
-                        continue;
-                    }
-                    float similarity = distributionTable.similarity(mainColor, chromosomeColor);
-                    petalPopulation.elementAt(i).insert(individual,similarity);
-                }else{
-                    System.out.println("Something is wrong PETAL");
-                }
-            }
+        int individual = 0;
+        for(int j=0; j<POPULATION_SIZE; j++){
+            individual = rand.nextInt(MAX_CHROMOSOME_VALUE + 1);
+            petalPopulation.add(individual);
         }
         rand = new Random();
-        for(int i=0; i<distributionTable.distributionCenter.size(); i++){
-            centerPopulation.add(new Population());
-            aptCenter.add(new Vector<>());
-            for(int j=0; j<POPULATION_SIZE; j++){
-                int individual = rand.nextInt(MAX_CHROMOSOME_VALUE + 1);
-                //System.out.println(individual);
-                Color chromosomeColor = distributionTable.distributionCenter.get(i).getValueOf(individual);
-                if(chromosomeColor!=null){
-                    Color mainColor = distributionTable.distributionCenter.get(i).mainColor();
-                    if(isMainColor(mainColor,chromosomeColor)){
-                        //aptCenter.add(chromosomeColor);
-                        aptCenter.elementAt(i).add(individual);
-                        continue;
-                    }
-                    float similarity = distributionTable.similarity(mainColor, chromosomeColor);
-                    centerPopulation.get(i).insert(individual,similarity);
-                }else{
-                    System.out.println("Something is wrong CENTER");
-                }
-            }
+        for(int j=0; j<POPULATION_SIZE; j++){
+            individual = rand.nextInt(MAX_CHROMOSOME_VALUE + 1);
+            centerPopulation.add(individual);
         }
     }
 
     public void evaluatePopulation(DistributionTable distributionTable){
-        for(int i=0; i<petalPopulation.size(); i++){
-            for(Integer value : petalPopulation.elementAt(i).population.keySet()){
-                if(isFitness(value, distributionTable.distributionPetal.elementAt(i))){
-                    System.out.println("Added P");
-                    //aptPetal.add(distributionTable.distributionPetal.elementAt(i).getValueOf(value));
-                    aptPetal.elementAt(i).add(value);
-                }
+        for(int value : centerPopulation){
+            if(isFitness(value, distributionTable.distributionCenter)){
+                aptCenter.add(value);
             }
         }
-        for(int i=0; i<centerPopulation.size(); i++){
-            for(Integer value : centerPopulation.elementAt(i).population.keySet()){
-                if(isFitness(value, distributionTable.distributionCenter.elementAt(i))){
-                    System.out.println("Added C");
-                    //aptCenter.add(distributionTable.distributionCenter.elementAt(i).getValueOf(value));
-                    aptCenter.elementAt(i).add(value);
-                }
+        for(int value : petalPopulation){
+            if(isFitness(value, distributionTable.distributionPetal)){
+                aptPetal.add(value);
             }
         }
     }
 
     public boolean isFitness(int fitnessValue, ColorNode colorNode){
-        if(fitnessValue <= APT_PERCENTAGE || colorNode.isMainColor(fitnessValue)){
+        if(colorNode.isMainColor(fitnessValue)){
+            return true;
+        }else if(colorNode.getSimilarity(fitnessValue) <= APT_PERCENTAGE){
             return true;
         }
         return false;
     }
 
-    public boolean isMainColor(Color color1, Color color2){
-        if(color1!=null && color2!=null){
-            if(color1.equals(color2)){
-                return true;
-            }
-        }
-        return false;
-    }
 
-    public void printPopulation(){
-        /*petalPopulation.printPopulation();
-        centerPopulation.printPopulation();*/
+    public void printPopulation(DistributionTable distributionTable){
+        for(int value : petalPopulation){
+            distributionTable.distributionPetal.printColor(value);
+        }
     }
 
     public Integer crossover(Integer firstParent, Integer secondParent){
         String binaryFirstParent = String.format("%16s",Integer.toBinaryString(firstParent)).replace(" ", "0");
         String binarySecondParent = String.format("%16s",Integer.toBinaryString(secondParent)).replace(" ", "0");
-
-        System.out.println(binaryFirstParent + " " + binarySecondParent);
         Random random = new Random();
-
         int intersectionPoint = random.nextInt(CHROMOSOMES_BITS);
-        int bitMask = MAX_CHROMOSOME_VALUE;
-
         String binaryChild = "";
         binaryChild += binaryFirstParent.substring(0, intersectionPoint);
         binaryChild += binarySecondParent.substring(intersectionPoint);
-
-        System.out.println("IP: " + intersectionPoint);
-        System.out.println("BC: " + binaryChild);
-
         return Integer.parseInt(binaryChild,2);
     }
 
@@ -143,7 +89,6 @@ public class GeneticAlgorithm implements ICONSTANTS {
             }
             mutatedIndividual += binaryIndividual.substring(shiftPosition+1);
             individual = Integer.parseInt(mutatedIndividual,2);
-
         }
         return individual;
     }
