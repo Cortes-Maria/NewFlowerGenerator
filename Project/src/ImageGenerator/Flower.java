70a99d55968b5d1f4d5cbb312a5833615d3c7a39
María José Cortés <maria2000jose.mj@gmail.com>
2020-07-13 18:44:23 -0600

    prints

--- Project/src/ImageGenerator/Flower.java
+++ Project/src/ImageGenerator/Flower.java
@@ -157,7 +157,7 @@ public class Flower extends JPanel implements ICONSTANTS {
         while (colors.size() <= POLYGON_PER_PETAL){
             for(Color color : controller.getPetalGenetic().getFitPopulationDistribution().keySet()){
                 int i = (int) (controller.getPetalGenetic().getFitPopulationDistribution().get(color).get(PERCENTAGE_INDEX)*CENTER_QUANTITY);
-                System.out.println(controller.getPetalGenetic().getFitPopulationDistribution().get(color).get(PERCENTAGE_INDEX));
+                //System.out.println(controller.getPetalGenetic().getFitPopulationDistribution().get(color).get(PERCENTAGE_INDEX));
                 while(i >= 0){
                     colors.add(color);
                     i--;
@@ -180,7 +180,7 @@ public class Flower extends JPanel implements ICONSTANTS {
         while (colors.size() <= CENTER_QUANTITY){
             for(Color color : controller.getCenterGenetic().getFitPopulationDistribution().keySet()){
                 int i = (int) (controller.getCenterGenetic().getFitPopulationDistribution().get(color).get(PERCENTAGE_INDEX)*CENTER_QUANTITY);
-                System.out.println(controller.getCenterGenetic().getFitPopulationDistribution().get(color).get(PERCENTAGE_INDEX));
+                //System.out.println(controller.getCenterGenetic().getFitPopulationDistribution().get(color).get(PERCENTAGE_INDEX));
                 while(i >= 0){
                     colors.add(color);
                     i--;
